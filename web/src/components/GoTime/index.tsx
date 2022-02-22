6f62a5c72480461689953bb381f27ed3711e3e7d
ClaytonWeller <clayton.weller@gmail.com>
2022-02-21 19:12:40 -0700

    Added HomeButton Component to go-time screen

--- web/src/components/GoTime/index.tsx
+++ web/src/components/GoTime/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { IStateProps } from '../../App';
+import HomeButton from '../shared/HomeButton';
 
-export default function GoTime({ transitionScreen }: IStateProps) {
+export default function GoTime({ transitionScreen, labor, updateLabor }: IStateProps) {
 
   return (
     <div>
       <div>GoTime</div>
-      <button onClick={() => { transitionScreen('home') }}>Home</button>
+      <HomeButton
+        transitionScreen={transitionScreen}
+        labor={labor}
+        updateLabor={updateLabor} />
     </div >
   );
 }
