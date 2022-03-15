b3f0dee86dd6b679244519c093d6e6441517654e
ClaytonWeller <clayton.weller@gmail.com>
2022-03-15 10:13:56 -0600

    Add schema validation to existing handlers

--- backend/app/handlers/labor/get.ts
+++ backend/app/handlers/labor/get.ts
@@ -1,9 +1,11 @@
 import { dynamo } from "../../integrations/dyanmo"
 import { withErrorHandling } from "../../utils/with-error-handling"
+import {z} from 'zod'
 
 async function getHandler(event) {
   const {userId} = event.queryStringParameters
-  const labors = await dynamo.labor.get(userId)
+  const validUserId = z.string().parse(userId)
+  const labors = await dynamo.labor.get(validUserId)
   return labors
 }
 
--- backend/app/handlers/labor/update.ts
+++ backend/app/handlers/labor/update.ts
@@ -1,10 +1,12 @@
 import { dynamo } from "../../integrations/dyanmo"
 import { calculateLaborValues } from "../../utils/calculate-labor-values"
 import { withErrorHandling } from "../../utils/with-error-handling"
+import {laborSchema} from '../../types/Labor'
 
 async function updateHandler(event) {
   const labor = JSON.parse(event.body)
-  const laborWithCalculations = calculateLaborValues(labor)
+  const validLabor = laborSchema.parse(labor)
+  const laborWithCalculations = calculateLaborValues(validLabor)
   const updatedLabor = await dynamo.labor.updateActive(laborWithCalculations)
   return updatedLabor
 }
